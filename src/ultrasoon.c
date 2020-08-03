acedea5d17364ca27f07b0f0ac639a5484cdc671
zepandasky <zepholleman2012@hotmail.nl>
2020-08-03 12:38:31 +0200

    first attempt

--- src/ultrasoon.c
+++ src/ultrasoon.c
@@ -23,6 +23,7 @@
 #include <string.h>
 
 volatile unsigned int pulse;
+volatile int overflow_counter = 0;
 
 
 void initUsart()
@@ -49,33 +50,58 @@ int main(void){
     EIMSK  |= (1 << INT0);
     sei(); /*enable global interrupts*/
 
-    OCR2A = 200;
-    TCCR2A |= (1 << COM2A0);
-    TCCR2A |= (1 << WGM21);
     TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20); /*Set 1024 prescalar*/
+    TIMSK2 |= (1 << TOIE2); /*Enables timer overflow interrupt*/
 
-    while(1){   
+    while(1){ 
+        /*Read Ultrasonic sensor values*/ 
         //Send 10uS pulse
         PORTB |= (1 << PB1);
         _delay_us(10);
         PORTB &= ~(1 << PB1);
-        int a;
 
         int16_t countA = ((pulse * 4) * 0.0340)/2; /*Calculate distance to object*/
         
         /*prints distance to object*/
-        char buffer[sizeof(unsigned int)];
-        itoa(countA,buffer,10);
-        for(int i =0; i < sizeof(buffer); i++)
-            transmitByte(buffer[i]);
+        // char buffer[sizeof(unsigned int)];
+        // itoa(countA,buffer,10);
+        // for(int i =0; i < sizeof(buffer); i++)
+        //     transmitByte(buffer[i]);
         
-        transmitByte('\n'); /*Print on new line*/
-        transmitByte('\r'); /*Print on beginning of new line*/
-    
-        _delay_ms(500);
+        // transmitByte('\n'); /*Print on new line*/
+        // transmitByte('\r'); /*Print on beginning of new line*/
+
+
+
     }
 }
+ISR(TIMER2_OVF_vect)
+{
+    overflow_counter++;
 
+    volatile int i = 0;
+    if(overflow_counter == 61 && i == 1)
+    {
+        TCCR2B &= ~(1 << CS22);
+        TCCR2B &= ~(1 << CS21);
+        TCCR2B &= ~(1 << CS20);
+    }
+    if(overflow_counter == 61 && i == 0)
+    {
+        TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20); /*Set 1024 prescalar*/
+        overflow_counter = 0;
+        i = 1;
+    }
+
+    char buffer[sizeof(unsigned int)];
+    itoa(overflow_counter,buffer,10);
+    for(int i =0; i < sizeof(buffer); i++)
+        transmitByte(buffer[i]);
+    
+    transmitByte('\n'); /*Print on new line*/
+    transmitByte('\r'); /*Print on beginning of new line*/
+   
+}
 ISR(INT0_vect){
     static volatile int i = 0;
     if(i == 1)
