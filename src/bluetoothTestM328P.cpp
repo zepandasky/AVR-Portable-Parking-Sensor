ae408a6e37778cf10b3aecb06df564d3ccf1d051
zepandasky <zepholleman2012@hotmail.nl>
2021-03-13 11:55:50 +0100

    wokring with hardware serial pins

--- src/bluetoothTestM328P.cpp
+++ src/bluetoothTestM328P.cpp
@@ -1,36 +1,32 @@
-/*
- * input.c
- *
- *  Created on: May 23, 2020
- *      Author: zep
- *  Description:
- *  Program to read values from ultra soon and send them through serial interface
- */
-#define true 1
+// #define true  1
+// #define false 0
 
-#include <Arduino.h>
-#include <NeoSWSerial.h>
+// #include <Arduino.h>
+// #include <NeoSWSerial.h>
 
-NeoSWSerial SoftSerial (11,12);    //PB2 en 3 op atmega
+// NeoSWSerial SoftSerial (6,7);    //PB2 en 3 op atmega
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Enter AT commands:");
-  SoftSerial.begin(9600);
+// void setup() {
+//   Serial.begin(9600);
+//   Serial.println("Enter AT commands:");
+//   SoftSerial.begin(9600);
 
-}
+// }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-    if(SoftSerial.available())
-        Serial.write(SoftSerial.read());
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//     if(SoftSerial.available())
+//         Serial.write(SoftSerial.read());
 
-    if(Serial.available())
-        SoftSerial.write(Serial.read());
+//     if(Serial.available()){
+//         SoftSerial.write(Serial.read());
+//         //Serial.write(Serial.read());
+//     }
+//     SoftSerial.write("test");
+//     // Serial.write("test");
 
-    SoftSerial.write("test");
-    _delay_ms(2000);
-}
+//     _delay_ms(100);
+// }
 
 
-//Note M328P = MASTER MODE ZETTEN DAN WERKT CONNECTIE AUTOMATISCH
+// Note M328P = MASTER MODE ZETTEN DAN WERKT CONNECTIE AUTOMATISCH
--- src/ultrasoon.cpp
+++ src/ultrasoon.cpp
@@ -1,16 +1,7 @@
-/*
- * input.c
- *
- *  Created on: May 23, 2020
- *      Author: zep
- *  Description:
- *  Program to read values from ultra soon and send them through serial interface
- */
 #ifndef BAUD
 #define BAUD 9600
 #endif
 
-#define F_CPU 16000000UL
 #define false 0
 #define true 1
 
@@ -23,53 +14,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-volatile int done = 0;
-
-
 #define START_TIMER TCCR1B |= (1 << CS10)
 #define STOP_TIMER TCCR1B &= ~(1 << CS10)
 
 volatile unsigned int icp_start = 0;
 volatile unsigned int icp_end = 0;
-volatile unsigned char edge = 0;
-
 volatile unsigned int pulseInUs = 0;
+volatile unsigned char edge = 0;
+volatile int done = 0;
 
-// void initUsart()
-// {
-//     UCSR0B |= (1 << TXEN0) | (1<<RXEN0);
-//     UCSR0C |= (1<<UCSZ00) | (1<<UCSZ01); //8 bit character size
-//     UBRR0H = UBRRH_VALUE;
-//     UBRR0L = UBRRL_VALUE;
-// }
+
+void initUsart()
+{
+    UCSR0B |= (1 << TXEN0) | (1<<RXEN0);
+    UCSR0C |= (1<<UCSZ00) | (1<<UCSZ01); //8 bit character size
+    UBRR0H = UBRRH_VALUE;
+    UBRR0L = UBRRL_VALUE;
+}
 
 // void transmitByte(uint8_t data){
 //   loop_until_bit_is_set(UCSR0A, UDRE0);
 //   UDR0 = data;
 // }
 
-void setInterrupts(){
-    TIMSK1 |= (1 << ICIE1); //Enable interrupts
+void setInterrupts()
+{
+    TCCR1B = 0x00;
     TCCR1B |= (1 << ICES1); //Positive rising edge capture
-    sei(); /*enable global interrupts*/
+    TIMSK1 |= (1 << ICIE1); //Enable interrupts
+    sei();                  /*enable global interrupts*/
 }
 
-
-int main(void){
-    // initUsart();
-    DDRB |=  (1<<PB2); /*PB2 als output instellen voor triggerpin*/
-    DDRB &= ~(1<<PB0); /*PB0 als input pin*/
-
-    PORTB  |= (1 << PB0);   /*Enable pull up resistor*/
-
-    setInterrupts();
+int main(void)
+{
+    //initUsart();
+    DDRB |= (1 << PB2);  /*PB2 als output instellen voor triggerpin*/
+    DDRB &= ~(1 << PB0); /*PB0 als input pin*/
 
     Serial.begin(9600);
 
-    while(1){ 
-        if(done == 0)
+    setInterrupts();
+
+    while (1)
+    {
+        if (done == 0)
         {
-            _delay_ms(60);
+            _delay_ms(300);
             PORTB |= (1 << PB2);
             _delay_us(10);
             PORTB &= ~(1 << PB2);
@@ -79,8 +69,10 @@ int main(void){
     }
 }
 
-ISR(TIMER1_CAPT_vect){
-    if(edge == 0){
+ISR(TIMER1_CAPT_vect)
+{
+    if (edge == 0)
+    {
         icp_start = ICR1;
         TCCR1B &= ~(1 << ICES1); //Negative falling edge
         edge = 1;
