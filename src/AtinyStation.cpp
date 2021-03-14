e12f17ff294a7f2cb48f268aeeb9fa82d766c73a
zepandasky <zepholleman2012@hotmail.nl>
2021-03-14 21:54:30 +0100

    added function to set beep frequency based on a parameter passed variable

--- src/AtinyStation.cpp
+++ src/AtinyStation.cpp
@@ -7,7 +7,6 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-#include <Arduino.h>
 
 #define RX 3 //PIN 2 on atiny
 #define TX 4 //PIn 3 on atiny
@@ -24,29 +23,53 @@
     FastPWM frequency = 8000000/(64*256) = 488hz
     Source: ATiny85 datasheet
 */
+void setBeepFrequency(int distance){
+    if(distance < 10)
+        TCCR1 |= (1 << CS13) | (1 << CS10); //PRESCALAR 256
+    else if(distance >= 10 && distance < 25)
+         TCCR1 |= (1 << CS13) | (1 << CS11);    //PRESCALAR 512
+    else if(distance >= 25 && distance <= 50)
+        TCCR1 |= (1 << CS13) | (1 << CS11) | (1 << CS10);   //PRESCALER 1024
+    else{ 
+        TCCR1 &= ~(1 << CS13) | ~(1 << CS12) | ~(1 << CS10); //SWITCH off timer
+        TCCR0B &= ~(1 << CS00);
+    }
+}
+void setTimer0_ENABLE()
+{
+    TCCR0A |= (1 << WGM00) | (1 << WGM01);   //FAST-PWM mode
+    TCCR0A |= (1 << COM0A0) | (1 << COM0A1); //SET on compare match, clear at bottom (TCNT0 == 0xff)
+    TCCR0B |= (1 << CS00);                   //64 prescalar
+    OCR0A = 120;                             //Defines
+}
 
-int main(void)
+void setTimer1_NO_ENABLE()
+{
+    OCR1A = 243;                                      // 1.0005763319672132 Hz (1000000/((243+1)*4096))
+    TCCR1 |= (1 << CTC1);                             // Enables CTC mode
+    TIMSK |= (1 << OCIE1A);                           //Enable output compare match interrupt
+}
+
+void resetTimerRegister()
 {
     TCNT0 = 0;
     TCNT1 = 0;
     TCCR1 = 0;
     TCCR0A = 0;
     TCCR0B = 0;
+}
 
-    DDRB = (1 << PB0) | (1 << PB2);
-    //Use pin pb0/pin5/0c0a
-    TCCR0A |= (1 << WGM00) | (1 << WGM01);   //FAST-PWM mode
-    TCCR0A |= (1 << COM0A0) | (1 << COM0A1); //SET on compare match, clear at bottom (TCNT0 == 0xff)
-    TCCR0B |= (1 << CS00);                   //64 prescalar
-    OCR0A = 120;                             //Defines
-
-    OCR1A = 243;                                      // 1.0005763319672132 Hz (1000000/((243+1)*4096))
-    TCCR1 |= (1 << CTC1);                             // Enables CTC mode
-    TCCR1 |= (1 << CS13) | (1 << CS12) | (1 << CS10); //PRESCALER 4096
-    TIMSK |= (1 << OCIE1A);                           //Enable output compare match interrupt
+int main(void)
+{
+    DDRB = (1 << PB0) | (1 << PB2); //set output pins
+    resetTimerRegister();
+    setTimer0_ENABLE();
+    setTimer1_NO_ENABLE();
     sei();
-
-    for (;;);
+    for (;;)
+    {
+        setBeepFrequency(40);
+    }
 };
 
 ISR(TIMER1_COMPA_vect)
